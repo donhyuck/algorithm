ccd4bf265ee341aeffa2dc3db28d31364c45b625
donhyuck <leedonhyuck123@gmail.com>
2022-11-25 20:42:13 +0900

    Apply .gitignore

--- code_test/src/sort/the_H_index/Solution.java
+++ code_test/src/sort/the_H_index/Solution.java
@@ -2,7 +2,7 @@ package sort.the_H_index;
 
 public class Solution {
 	public static void main(String[] args) {
-		
+
 		Solution test = new Solution();
 		int[] array = new int[] { 3, 0, 6, 1, 5 };
 		int result = test.solution(array);
