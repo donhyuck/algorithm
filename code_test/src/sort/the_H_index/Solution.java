6aa783bef13d1e19d59e8cea00e73b1c4a09dcd8
donhyuck <leedonhyuck123@gmail.com>
2022-11-25 20:30:50 +0900

    정렬 - H-index

---
+++ code_test/src/sort/the_H_index/Solution.java
@@ -0,0 +1,17 @@
+package sort.the_H_index;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		Solution test = new Solution();
+		int[] array = new int[] { 3, 0, 6, 1, 5 };
+		int result = test.solution(array);
+
+		System.out.println(result);
+	}
+
+	public int solution(int[] citations) {
+		int answer = 0;
+		return answer;
+	}
+}
